41ae58ed2242df0f16548a980bc4aab557b1c298
Julius Hsu <juliushsu07@gmail.com>
2022-12-08 18:51:54 -0500

    first commit: Create profile and login page with React PassageID component.

---
+++ frontend/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '@passageidentity/passage-elements/passage-auth'
+
+function Profile() {
+  return (
+    <div className='container'>
+        <h1>Passage Demo</h1>
+        <passage-auth app-id={process.env.REACT_APP_PASSAGE_APP_ID}></passage-auth>
+    </div>
+  )
+}
+
+export default Profile
