1d9fbf0c2dcb021dc9fd9bc0b8fe0f50967f9119
Julius Hsu <juliushsu07@gmail.com>
2022-12-08 19:47:36 -0500

    Create login useCurrentUser hook to authenticate user with email link.

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useCurrentUser } from '../hooks/useCurrentUser'
 
 function Profile() {
+  const { isLoading, isAuthorized, username } = useCurrentUser();
+  
+  if (isLoading) return null;
+
+  if(!isAuthorized) {
+    console.log(isAuthorized)
+    return <p>You must <linked to="/">log in</linked> to view this page!</p>;
+  }
+
   return (
     <div>
-        <p>You are logged in!</p>
+        <p>You are logged in as <strong>{username}</strong>!</p>
     </div>
   )
 }
---
+++ frontend/src/hooks/useCurrentUser.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import { PassageUser } from '@passageidentity/passage-elements/passage-user'
+
+export function useCurrentUser() {
+    const [result, setResult] = useState({
+        isLoadiong: true,
+        isAuthorized: false,
+        username: ""
+    })
+
+    useEffect(() => {
+        let cancelRequest = false;
+        new PassageUser().userInfo().then((userInfo) => {
+            if (cancelRequest) return;
+
+            if (userInfo === undefined) {
+                setResult( {
+                    isLoadiong: false,
+                    isAuthorized: false,
+                    username: "",
+                });
+                return;
+            }
+            setResult({
+                isLoadiong: false,
+                isAuthorized: true,
+                username: userInfo.email ? userInfo.email : userInfo.phone,
+            });
+        });
+        return () => {
+            cancelRequest = true;
+        }
+    }, []);
+    
+    console.log(result);
+
+    return result;
+    
+}
